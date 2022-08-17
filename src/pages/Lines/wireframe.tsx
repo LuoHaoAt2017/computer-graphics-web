d648707ffcacda48d565d0eaf2a4e7b7a14dcd06
洛神赋 <hluo5206@gmail.com>
2022-08-17 20:50:54 +0800

    feat: 轨道控制器和线框几何体

--- src/pages/Lines/wireframe.tsx
+++ src/pages/Lines/wireframe.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  PointLight,
   AmbientLight,
   WebGLRenderer,
   PerspectiveCamera,
@@ -19,7 +18,7 @@ import {
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // 点光源
-class PointLightDemo extends React.Component {
+class WireframeDemo extends React.Component {
   private scene = new Scene();
   private camera = new PerspectiveCamera(
     45,
@@ -27,54 +26,45 @@ class PointLightDemo extends React.Component {
     1,
     1000,
   );
-  private pointRedLight = new PointLight(0xff0000, 1);
-  private pointBlueLight = new PointLight(0x0000ff, 1);
   private renderer = new WebGLRenderer({ antialias: true });
+  private controls = new OrbitControls(this.camera, this.renderer.domElement);
   private ref = React.createRef<HTMLDivElement>();
-  private R = 40;
-
-  constructor(props) {
-    super(props);
-    this.initializeRenderer();
-    this.initializeCamera();
-  }
 
   render(): React.ReactNode {
     return <div ref={this.ref}></div>;
   }
 
   componentDidMount() {
+    this.initMainScene();
+    this.initializeCamera();
     this.initialzeObjects();
     this.initialzePointLight();
     this.intialzeGround();
-    const controls = new OrbitControls(this.camera, this.renderer.domElement);
-    this.camera.lookAt(this.scene.position);
+    this.onWindowResize();
+    this.initControls();
+    this.renderer.render(this.scene, this.camera);
     this.ref.current?.appendChild(this.renderer.domElement);
-
-    let radian = 0.01;
-    const speed = 0.02;
     const draw = () => {
       requestAnimationFrame(draw);
-      controls.update();
-      radian = radian + speed;
-      this.pointRedLight.position.x = Math.sin(radian) * (this.R + 20);
-      this.pointRedLight.position.y = this.R;
-      this.pointRedLight.position.z = Math.cos(radian) * (this.R + 20);
-
-      this.pointBlueLight.position.x = Math.cos(radian) * (this.R + 20);
-      this.pointBlueLight.position.y = this.R;
-      this.pointBlueLight.position.z = Math.sin(radian) * (this.R + 20);
-
+      this.controls.update();
       this.renderer.render(this.scene, this.camera);
     };
 
     draw();
   }
 
-  initializeRenderer() {
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  initMainScene() {
+    this.camera.lookAt(this.scene.position);
     this.renderer.setClearColor(0x000000);
-    this.renderer.shadowMap.enabled = true;
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  initControls() {
+    const controls = this.controls;
+    controls.minDistance = 10;
+    controls.maxDistance = 500;
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 4.0;
   }
 
   initializeCamera() {
@@ -97,7 +87,7 @@ class PointLightDemo extends React.Component {
       linewidth: 5,
       linecap: 'round',
     });
-    const line = new LineSegments(wireframe, material); // 网格几何体
+    const line = new LineSegments(wireframe, material); // 网格几何体 在若干对的顶点之间绘制的一系列的线。
     line.position.x = 0;
     line.position.y = 40;
     line.position.z = 0;
@@ -118,6 +108,16 @@ class PointLightDemo extends React.Component {
     ground.rotateX(Math.PI / 2); //
     this.scene.add(ground);
   }
+
+  onWindowResize() {
+    window.addEventListener('resize', () => {
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
+    });
+  }
 }
 
-export default PointLightDemo;
+export default WireframeDemo;
